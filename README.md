e6a4f5dbdab094c0e3d74298095dafce2cf4a9c1
raichu-CS <RaiChuCS@outlook.com>
2026-01-31 20:47:49 -0800

    New Repository Created!

---
+++ README.md
@@ -0,0 +1,12 @@
+CS2400 Repository
+
+Name: Raileigh Riel Chua
+
+GitHub Repository Link:
+https://github.com/RaiChu-CS/CS2400-RC-Repository
+
+https://github.com/RaiChu-CS/CS2400-RC-Repository.git
+
+Video Link:
+
+Recorder Version/Application:
---
+++ java-demo/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //prints Hello World to terminal
+
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum of a and b is: " + sum);
+    }
+}
